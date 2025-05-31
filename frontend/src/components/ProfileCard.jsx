db588ac9b1db2e0bd94709c5fdfb3e260bec0512
HaseethVardhan <haseethvardhan01@gmail.com>
2025-05-31 22:39:04 +0530

    completed frontend for profile card on homepage.

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -1,9 +1,89 @@
 import React from 'react'
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
-const ProfileCard = () => {
+const ProfileCard = ({ id, url, setCards, cards }) => {
+  const x = useMotionValue(0);
+  
+  const rotateRaw = useTransform(x, [-150, 150], [-18, 18]);
+  const opacity = useTransform(x, [-150, 0, 150], [0.7, 1, 0.7]);
+  const rotate = rotateRaw;
+
+  const isFront = id === cards[cards.length - 1].id;
+
+  const handleDragEnd = () => {
+    if (Math.abs(x.get()) > 100) {
+      setCards((pv) => pv.filter((v) => v.id !== id));
+    }
+  };
+
   return (
-    <div>ProfileCard</div>
-  )
-}
+    <motion.div
+      className="h-130 w-88 origin-bottom rounded-lg bg-[#333333] hover:cursor-grab active:cursor-grabbing px-3"
+      style={{
+        gridRow: 1,
+        gridColumn: 1,
+        x,
+        opacity,
+        rotate,
+        transition: "0.125s transform",
+        boxShadow: isFront
+          ? "0 20px 25px -5px rgb(0 0 0 / 0.5), 0 8px 10px -6px rgb(0 0 0 / 0.5)"
+          : undefined,
+        zIndex: isFront ? 1 : 0
+      }}
+      animate={{
+        scale: isFront ? 1 : 0.98,
+      }}
+      drag={isFront ? "x" : false}
+      dragConstraints={{
+        left: 0,
+        right: 0,
+      }}
+      onDragEnd={handleDragEnd}
+    >
+      <div className='h-60 w-full mt-3'>
+        <img 
+        src={url} 
+        alt="Placeholder alt"
+        className="h-full w-full object-cover rounded-lg"
+      />
+      </div>
+      <div className='flex flex-row items-center justify-between px-1 py-2'>
+        <div className='flex flex-col items-start justify-between w-[50%]'>
+          <div className='font-inter font-[500] text-sm text-[#aaaaaa]'>Name</div>
+          <div className='font-inter font-[600] text-base text-white'>James</div>
+        </div>
+        <div className='flex flex-col items-end justify-between w-[50%]'>
+          <div className='font-inter font-[500] text-sm text-[#aaaaaa]'>Profession</div>
+          <div className='font-inter font-[600] text-base text-white'>Product Designer</div>
+        </div>
+      </div>
+      <div className='flex flex-col items-start gap-2 justify-between px-1 '>
+        <div className='font-inter font-[500] text-sm text-[#aaaaaa]'>Key Skills :</div>
+        <div className='flex flex-row flex-wrap gap-2'>
+          <div className='cursor-pointer font-inter font-[500] text-[13px] tracking-[0.5px] flex items-center justify-center text-center px-1 py-1 rounded-sm bg-[#6b6b6b] text-[#f4f4f4]'>
+            Product Management
+          </div>
+          <div className='cursor-pointer font-inter font-[500] text-[13px] tracking-[0.5px] flex items-center justify-center text-center px-1 py-1 rounded-sm bg-[#6b6b6b] text-[#f4f4f4]'>
+            Product Management
+          </div>
+          <div className='cursor-pointer font-inter font-[500] text-[13px] tracking-[0.5px] flex items-center justify-center text-center px-1 py-1 rounded-sm bg-[#6b6b6b] text-[#f4f4f4]'>
+            Product Management
+          </div>
+          <div className='cursor-pointer font-inter font-[500] text-[13px] tracking-[0.5px] flex items-center justify-center text-center px-1 py-1 rounded-sm bg-[#6b6b6b] text-[#f4f4f4]'>
+            Product Management
+          </div>
+          <div className='cursor-pointer font-inter font-[500] text-[13px] tracking-[0.5px] flex items-center justify-center text-center px-1 py-1 rounded-sm bg-[#6b6b6b] text-[#f4f4f4]'>
+            Product Management
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-row items-center justify-end px-1 gap-3'>
+        <img className='h-[32px] w-[32px] object-contain' src="https://res.cloudinary.com/dbzcsfi3e/image/upload/v1748710879/iconamoon_heart-light_jililb.png" />
+        <img className='h-[22px] w-[22px] object-contain' src="https://res.cloudinary.com/dbzcsfi3e/image/upload/v1748710879/Vector_4_kq2v16.png"/>
+      </div>
+    </motion.div>
+  );
+};
 
 export default ProfileCard
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 import BottomNavbar from '../components/BottomNavbar'
 import ProfileCard from '../components/ProfileCard'
 
 const Home = () => {
+
+  const cardData = [
+  {
+    id: 1,
+    url: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    url: "https://images.unsplash.com/photo-1512374382149-233c42b6a83b?q=80&w=2235&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    url: "https://images.unsplash.com/photo-1539185441755-769473a23570?q=80&w=2342&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    url: "https://images.unsplash.com/photo-1549298916-b41d501d3772?q=80&w=2224&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 5,
+    url: "https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 6,
+    url: "https://images.unsplash.com/photo-1570464197285-9949814674a7?q=80&w=2273&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 7,
+    url: "https://images.unsplash.com/photo-1578608712688-36b5be8823dc?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 8,
+    url: "https://images.unsplash.com/photo-1505784045224-1247b2b29cf3?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+const [cards, setCards] = useState(cardData);
+
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col relative h-screen'>
       <div className='flex flex-row items-center justify-between px-6 py-10'>
         <div className='font-poppins font-[700] text-3xl text-white'>Partnr</div>
         <div><img src="https://res.cloudinary.com/dbzcsfi3e/image/upload/v1748694120/Vector_1_qwnwxs.png" /></div>
       </div>
-      <ProfileCard />
-      <BottomNavbar current='home'/>
+      <div className="grid h-[500px] py-6 w-full place-items-center">
+      {cards.map((card) => {
+        return (
+          <ProfileCard key={card.id} cards={cards} setCards={setCards} {...card} />
+        );
+      })}
+    </div>
+    <BottomNavbar current='home'/>
     </div>
   )
 }
